29c4e155916251f11b2252a9f3f8f8e45607945d
Berkaygulmez <berkaybjk04@icloud.com>
2023-05-16 17:00:50 +0300

    Admin panelde Slider CRUD işlemleri tamamlandı

---
+++ E_Ticaret_Project/Areas/Admin/Controllers/SliderController.cs
@@ -0,0 +1,66 @@
+﻿using E_Ticaret_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace E_Ticaret_Project.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SliderController : Controller
+    {
+        private readonly MyDbContext _baglanti;
+
+        public SliderController(MyDbContext context)
+        {
+            _baglanti = context;
+        }
+
+        public IActionResult Slider()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult SliderList()
+        {
+            var sliderList = _baglanti.HomeSliders.ToList(); //normal verileri çektim
+            return Json(sliderList); //json formatında gönderdim
+        }
+
+        [HttpGet]
+        public JsonResult GetSliderById(int id)
+        {
+            var tekSliderVerisi = _baglanti.HomeSliders.Where(x => x.SliderID == id).FirstOrDefault();
+            return Json(tekSliderVerisi); //json formatında gönderdim
+        }
+
+        [HttpPost]
+        public JsonResult SliderAdd(HomeSlider homeSlider)
+        {
+            _baglanti.HomeSliders.Add(homeSlider);
+            _baglanti.SaveChanges();
+
+            return Json(new { success = true });
+        }
+
+        [HttpPost]
+        public JsonResult SliderUpdate(HomeSlider HomeSlider)
+        {
+            _baglanti.HomeSliders.Update(HomeSlider);
+            _baglanti.SaveChanges();
+
+            return Json(new { success = true });
+        }
+
+        [HttpPost]
+        public JsonResult SliderDelete(int SliderID)
+        {
+            var silinecekSlider = _baglanti.HomeSliders.Find(SliderID);
+
+            _baglanti.HomeSliders.Remove(silinecekSlider);
+            _baglanti.SaveChanges();
+
+            return Json(new { success = true });
+        }
+
+
+    }
+}

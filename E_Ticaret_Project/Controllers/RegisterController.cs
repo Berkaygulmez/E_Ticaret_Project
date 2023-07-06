e7ed535e69a42d512561ffebef13503b7574457f
Berkaygulmez <berkaybjk04@icloud.com>
2023-07-06 15:55:46 +0300

    Kayıt işlemleri genel olarak tamamlandı.

--- E_Ticaret_Project/Controllers/RegisterController.cs
+++ E_Ticaret_Project/Controllers/RegisterController.cs
@@ -1,12 +1,50 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using E_Ticaret_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace E_Ticaret_Project.Controllers
 {
     public class RegisterController : Controller
     {
+
+        private readonly MyDbContext _baglanti;
+
+        public RegisterController(MyDbContext context)
+        {
+            _baglanti = context;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+     
+        [HttpPost]
+        public ActionResult Index(Register model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Veritabanına kaydetme işlemleri burada yapılabilir
+
+                //var mailVarMı = sql e gidip mailadresi var mı diye bakacak linq kodu count dönderecek yani sayı
+
+                //if mailVarMı > 0  dan büyükse kaydetmesin çünkü o mail adresi kullanılmış else ise kaydetsin kaydetme kodları zaten aşağıda
+                if (_baglanti.Registers.Any(r => r.Mail == model.Mail))
+                {
+                    ModelState.AddModelError("", "Bu e-posta adresi zaten kullanımda.");
+                    TempData["ErrorMessage"] = "Bu e-posta adresi zaten kullanımda.";
+                    return RedirectToAction("Index");
+                }
+
+                _baglanti.Registers.Add(model);
+                _baglanti.SaveChanges();
+
+                // Kayıt işlemi başarılı olduğunda, giriş sayfasına yönlendirin
+                return RedirectToAction("Index", "Home");
+            }
+
+            // ModelState.IsValid false olduğunda, hata mesajlarıyla birlikte kayıt sayfasını tekrar gösterin
+            return View(model);
         }
     }
 }
--- E_Ticaret_Project/Models/Register.cs
+++ E_Ticaret_Project/Models/Register.cs
@@ -9,7 +9,6 @@ namespace E_Ticaret_Project.Models
         public int RegisterID { get; set; } //bu olmalı bence ama hata bundan mı emin değilim
         public string NameSurname { get; set; }
         public string Password { get; set; }
-        public string ConfirmPassword { get; set; }
         public string Mail { get; set; }
         public string UserName { get; set; }
     }
